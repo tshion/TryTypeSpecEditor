42ea072defe21f334f2db0fa5cdb273c41f20128
tshion <tenta_shion@yahoo.co.jp>
2024-09-04 21:33:05 +0900

    サイドメニューの仮実装

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { schemaData } from '../schema';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,25 @@ import { Component } from '@angular/core';
   imports: [],
   template: `
     <nav class="pure-menu pure-menu-scrollable">
-      aaa
+      <hgroup>
+        <h1>TypeSpecEditor</h1>
+        {{ appVersion }}
+      </hgroup>
+      <hr />
+      <form class="pure-form pure-form-stacked">
+        <input type="file" accept="application/json" class="pure-input-1" />
+
+        <label for="meta-title">タイトル</label>
+        <input type="text" id="meta-title" class="pure-input-1" />
+
+        <button type="button" class="pure-button">ファイル出力</button>
+      </form>
+      <hr />
+      <ul class="pure-menu-list">
+        <li class="pure-menu-item">
+          <a href="https://www.google.com/maps" class="pure-menu-link">Google Map</a>
+        </li>
+      </ul>
     </nav>
     <main>
       bbb
@@ -23,15 +42,27 @@ import { Component } from '@angular/core';
       color: var(--app-color-base-on);
       flex-grow: 1;
       height: 100vh;
-      overflow-y: scroll;
+      overflow-y: auto;
     }`,
     `nav {
       background-color: var(--app-color-main);
       color:  var(--app-color-main-on);
       height: 100vh;
+      overflow-y: auto;
       width: 360px;
+    }`,
+    `nav > form {
+      padding: 8px 16px;
+    }`,
+    `nav > hgroup {
+      padding: 16px;
+    }`,
+    `nav > hgroup > h1 {
+      margin: 0 0 8px 0;
     }`,
   ],
 })
 export class AppComponent {
+
+  public readonly appVersion = schemaData.version;
 }
